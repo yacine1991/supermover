755e8ce9fd4fabd03c45789713537949ba4ca573
Yacine Ma <yacine1991@gmail.com>
2015-07-08 15:18:15 +0200

    Create supermover.sh

---
+++ supermover.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Solution to solve the problem of too argument list
+echo "supermover"
+
+if [[ $# -eq 0 ]]
+then
+  echo " No arguments supplied "
+  echo "USAGE : toolong [typeofcommand : mv cp rm] [fileextension] [source] [destination]"
+fi
+
+find  $3 -name "*.$2" -exec $1  "{}" $4 \;
+
+#trying to find a function which display the time left to finish the copy
+
+echo "Done"
